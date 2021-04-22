fb4ca61d92495f616d2658f001312db761b9263f
WHWH10 <eyet010@gmail.com>
2021-04-22 09:12:14 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+const config = require('./config/key')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json 
+app.use(bodyParser.json());
+
+// const mongoose = require('mongoose')
+// mongoose.connect('mongodb://eunji:eunji@localhost:27017/testDB', {
+//     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+// }).then(() => console.log('MongoDB Connect...'))
+//     .catch(err => console.log(err))
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/testDB', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connect...'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('THIS IS INDEX PAGE:)')
+})
+
+app.post('/register', (req, res) => {
+    // 회원가입 시 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
